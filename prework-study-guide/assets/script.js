8d123db0dc9fb0f1410a7d87e006b79233fd8336
Wolffkran <liebherr2008@live.com>
2023-08-08 19:38:52 -0400

    Your commit message here

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1c2ac88d6a4cac732424da17494b6f56a06e2dcf
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
